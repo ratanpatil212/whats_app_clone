a64efbab071f3840e8b4e9b4852ee65fa31c321b
ratanpatil212 <ratanpatil212@gmail.com>
2022-07-01 18:48:39 +0530

    Added Camera and subsections

---
+++ lib/scripts/chatspage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class chatspage extends StatefulWidget {
+  const chatspage({Key? key}) : super(key: key);
+
+  @override
+  State<chatspage> createState() => _chatspageState();
+}
+
+class _chatspageState extends State<chatspage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/scripts/home.dart
+++ lib/scripts/home.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    Color color1 = HexColor("#128C7E");
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
-        title: Text('Whatsapp'),
-        backgroundColor: color1,
+        title: Text(
+          'Whatsapp',
+          style: GoogleFonts.montserrat(
+            textStyle: TextStyle(color: Colors.white, letterSpacing: .5),
+          ),
+        ),
+        backgroundColor: HexColor("#075E54"),
         actions: <Widget>[
           Padding(
             padding: EdgeInsets.only(right: 20.0),
@@ -35,35 +40,68 @@ class HomePage extends StatelessWidget {
         ],
       ),
       body: Container(
-        color: color1,
+        color: HexColor("#075E54"),
         child: Row(
           children: [
             Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 15.0, 15.0, 15.0),
+              padding: EdgeInsets.fromLTRB(15, 12, 1, 8),
               child: GestureDetector(
                 onTap: () {},
-                child: Icon(
+                child: const Icon(
                   Icons.camera_alt_rounded,
                   color: Colors.white,
-                  size: 30,
+                  size: 25,
                 ),
               ),
             ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(15.0, 15.0, 15.0, 15.0),
-              child: GestureDetector(
-                onTap: () {},
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: "Chats",
-                  ),
-                ),
-              ),
-            ),
+            // Container(
+            //   width: MediaQuery.of(context).size.width * (1 / 7),
+            //   color: HexColor("#075E54"),
+            //   child: Material(
+            //     child: const Icon(
+            //       Icons.camera_alt_rounded,
+            //       color: Colors.black,
+            //       size: 20,
+            //     ),
+            //   ),
+            // ),
+            subsection(subname: 'CHATS'),
+            subsection(subname: 'STATUS'),
+            subsection(subname: 'CALLS'),
           ],
         ),
       ),
+    );
+  }
+}
+
+class subsection extends StatelessWidget {
+  final String subname;
+  const subsection({required this.subname});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        width: MediaQuery.of(context).size.width * (2 / 7),
+        padding: EdgeInsets.fromLTRB(1, 12, 1, 0),
+        height: 40,
+        child: Text(
+          subname,
+          textAlign: TextAlign.center,
+          style: GoogleFonts.montserrat(
+            textStyle: TextStyle(color: Colors.white, letterSpacing: .5),
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+      onTap: () {
+        Fluttertoast.showToast(
+            msg: subname + " was pressed",
+            toastLength: Toast.LENGTH_SHORT,
+            fontSize: 18.0);
+      },
     );
   }
 }
